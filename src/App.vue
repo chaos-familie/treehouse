885849e04fa08f56a4737b09e5338e30cbabf6d0
Turboman3000 <turboman@turboman.de>
2025-11-02 21:24:17 +0100

    feat: initial project setup
    Adds base files and dependencies for a Vue project.
    Includes eslint, prettier, tailwind, and nuxt ui.

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <TolgeeProvider>
+    <UTooltipProvider>
+      <UToaster>
+        <UApp>
+          <UContainer class="gap-4 min-h-screen flex flex-col">
+              <RouterView />
+          </UContainer>
+        </UApp>
+        </UToaster>
+      </UTooltipProvider>
+  </TolgeeProvider>
+</template>
