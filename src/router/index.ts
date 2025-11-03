b59c6d46dc4b9e9b52e89a570098adb9d803c25c
Turboman3000 <turboman@turboman.de>
2025-11-03 20:24:29 +0100

    feat: add legal views and routes
    Adds privacy and impress views and their corresponding routes.

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,8 @@ import BlogOverview from '@/views/blog/BlogOverviewView.vue'
 import BlogPage from '@/views/blog/BlogView.vue'
 import FamilyTreeView from '@/views/FamilyTreeView.vue'
 import HomeView from '@/views/HomeView.vue'
+import ImpressView from '@/views/legal/ImpressView.vue'
+import PrivacyView from '@/views/legal/PrivacyView.vue'
 import MemberListView from '@/views/members/MemberListView.vue'
 import MembersView from '@/views/members/MemberView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
@@ -17,6 +19,9 @@ const router = createRouter({
     { path: '/family-tree', name: 'family_tree', component: FamilyTreeView },
     { path: '/m/:name', name: 'member', component: MembersView },
     { path: '/members', name: 'member_list', component: MemberListView },
+
+    { path: '/legal/privacy', name: 'privacy', component: PrivacyView },
+    { path: '/legal/impress', name: 'impress', component: ImpressView },
 
     { path: '/:pathMatch(.*)', redirect: '/' },
   ],
